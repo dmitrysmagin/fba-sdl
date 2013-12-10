e642bb4649c087506e38e5aeab9c8ef2be4ef6f7
Dmitry Smagin <exmortis@yandex.ru>
2013-12-11 00:59:57 +0400

    GCW0: Save both options and keymap in *romname*.cfg

--- src/sdl-dingux/config.cpp
+++ src/sdl-dingux/config.cpp
@@ -130,33 +130,42 @@ int ConfigGameLoad()
 
 	sprintf((char*)cfgname, "%s/%s.cfg", szAppConfigPath, BurnDrvGetTextA(DRV_NAME));
 
-	if ((f = fopen(cfgname,"r")) == NULL) {
+	if(!(f = fopen(cfgname,"r"))) {
 		// set default values and exit
+		ConfigGameDefault();
 		return 0;
 	}
-
-	if (!f) return 0;
 
 	while(fgets(line,sizeof(line),f) != NULL) {
 		sscanf(line, "%s %d", &arg1, &argd);
 
-		if (argd > 0) {
-			if (strcmp(arg1,"#") != 0) {
-				if (strcmp(arg1, "KEY_UP") == 0) keymap.up = argd;
-				if (strcmp(arg1, "KEY_DOWN") == 0) keymap.down = argd;
-				if (strcmp(arg1, "KEY_LEFT") == 0) keymap.left = argd;
-				if (strcmp(arg1, "KEY_COIN1") == 0) keymap.coin1 = argd;
-				if (strcmp(arg1, "KEY_START1") == 0) keymap.start1 = argd;
-				if (strcmp(arg1, "KEY_RIGHT") == 0) keymap.right = argd;
-				if (strcmp(arg1, "KEY_FIRE1") == 0) keymap.fire1 = argd;
-				if (strcmp(arg1, "KEY_FIRE2") == 0) keymap.fire2 = argd;
-				if (strcmp(arg1, "KEY_FIRE3") == 0) keymap.fire3 = argd;
-				if (strcmp(arg1, "KEY_FIRE4") == 0) keymap.fire4 = argd;
-				if (strcmp(arg1, "KEY_FIRE5") == 0) keymap.fire5 = argd;
-				if (strcmp(arg1, "KEY_FIRE6") == 0) keymap.fire6 = argd;
-				if (strcmp(arg1, "KEY_QUIT") == 0) keymap.quit = argd;
-				if (strcmp(arg1, "KEY_PAUSE") == 0) keymap.pause = argd;
-			}
+		if(strcmp(arg1, "#") != 0) {
+			if(strcmp(arg1, "FBA_SOUND") == 0) options.sound = argd;
+			if(strcmp(arg1, "FBA_SAMPLERATE") == 0) options.samplerate = argd;
+			if(strcmp(arg1, "FBA_VSYNC") == 0) options.vsync = argd;
+			if(strcmp(arg1, "FBA_RESCALE") == 0) options.rescale = argd;
+			if(strcmp(arg1, "FBA_ROTATE") == 0) options.rotate = argd;
+			if(strcmp(arg1, "FBA_SHOWFPS") == 0) options.showfps = argd;
+			if(strcmp(arg1, "FBA_FRAMESKIP") == 0) options.frameskip = argd;
+			if(strcmp(arg1, "FBA_M68KCORE") == 0) options.m68kcore = argd;
+			if(strcmp(arg1, "FBA_Z80CORE") == 0) options.z80core = argd;
+			if(strcmp(arg1, "FBA_SENSE") == 0) options.sense = argd;
+			if(strcmp(arg1, "FBA_USESWAP") == 0) options.useswap = argd;
+
+			if(strcmp(arg1, "KEY_UP") == 0) keymap.up = argd;
+			if(strcmp(arg1, "KEY_DOWN") == 0) keymap.down = argd;
+			if(strcmp(arg1, "KEY_LEFT") == 0) keymap.left = argd;
+			if(strcmp(arg1, "KEY_COIN1") == 0) keymap.coin1 = argd;
+			if(strcmp(arg1, "KEY_START1") == 0) keymap.start1 = argd;
+			if(strcmp(arg1, "KEY_RIGHT") == 0) keymap.right = argd;
+			if(strcmp(arg1, "KEY_FIRE1") == 0) keymap.fire1 = argd;
+			if(strcmp(arg1, "KEY_FIRE2") == 0) keymap.fire2 = argd;
+			if(strcmp(arg1, "KEY_FIRE3") == 0) keymap.fire3 = argd;
+			if(strcmp(arg1, "KEY_FIRE4") == 0) keymap.fire4 = argd;
+			if(strcmp(arg1, "KEY_FIRE5") == 0) keymap.fire5 = argd;
+			if(strcmp(arg1, "KEY_FIRE6") == 0) keymap.fire6 = argd;
+			if(strcmp(arg1, "KEY_QUIT") == 0) keymap.quit = argd;
+			if(strcmp(arg1, "KEY_PAUSE") == 0) keymap.pause = argd;
 		}
 	}
 
@@ -174,20 +183,36 @@ int ConfigGameSave()
 	fp = fopen(cfgname, "w");
 	if(!fp) return 0;
 
-	fprintf(fp,"KEY_UP %d\n", keymap.up);
-	fprintf(fp,"KEY_DOWN %d\n", keymap.down);
-	fprintf(fp,"KEY_LEFT %d\n", keymap.left);
-	fprintf(fp,"KEY_RIGHT %d\n", keymap.right);
-	fprintf(fp,"KEY_COIN1 %d\n", keymap.coin1); 
-	fprintf(fp,"KEY_START1 %d\n", keymap.start1);
-	fprintf(fp,"KEY_FIRE1 %d\n", keymap.fire1);
-	fprintf(fp,"KEY_FIRE2 %d\n", keymap.fire2);
-	fprintf(fp,"KEY_FIRE3 %d\n", keymap.fire3);
-	fprintf(fp,"KEY_FIRE4 %d\n", keymap.fire4);
-	fprintf(fp,"KEY_FIRE5 %d\n", keymap.fire5);
-	fprintf(fp,"KEY_FIRE6 %d\n", keymap.fire6);
-	fprintf(fp,"KEY_QUIT %d\n", keymap.quit);
-	fprintf(fp,"KEY_PAUSE %d\n", keymap.pause);
+	fprintf(fp, "# FBA config file\n");
+	fprintf(fp, "# %s\n\n", BurnDrvGetTextA(DRV_NAME));
+
+	fprintf(fp, "FBA_SOUND %d\n", options.sound);
+	fprintf(fp, "FBA_SAMPLERATE %d\n", options.samplerate);
+	fprintf(fp, "FBA_VSYNC %d\n", options.vsync);
+	fprintf(fp, "FBA_RESCALE %d\n", options.rescale);
+	fprintf(fp, "FBA_ROTATE %d\n", options.rotate);
+	fprintf(fp, "FBA_SHOWFPS %d\n", options.showfps);
+	fprintf(fp, "FBA_FRAMESKIP %d\n", options.frameskip);
+	fprintf(fp, "FBA_M68KCORE %d\n", options.m68kcore);
+	fprintf(fp, "FBA_Z80CORE %d\n", options.z80core);
+	fprintf(fp, "FBA_SENSE %d\n", options.sense);
+	fprintf(fp, "FBA_USESWAP %d\n", options.useswap);
+
+	fprintf(fp, "\n# Keys layout\n\n");
+	fprintf(fp, "KEY_UP %d\n", keymap.up);
+	fprintf(fp, "KEY_DOWN %d\n", keymap.down);
+	fprintf(fp, "KEY_LEFT %d\n", keymap.left);
+	fprintf(fp, "KEY_RIGHT %d\n", keymap.right);
+	fprintf(fp, "KEY_COIN1 %d\n", keymap.coin1); 
+	fprintf(fp, "KEY_START1 %d\n", keymap.start1);
+	fprintf(fp, "KEY_FIRE1 %d\n", keymap.fire1);
+	fprintf(fp, "KEY_FIRE2 %d\n", keymap.fire2);
+	fprintf(fp, "KEY_FIRE3 %d\n", keymap.fire3);
+	fprintf(fp, "KEY_FIRE4 %d\n", keymap.fire4);
+	fprintf(fp, "KEY_FIRE5 %d\n", keymap.fire5);
+	fprintf(fp, "KEY_FIRE6 %d\n", keymap.fire6);
+	fprintf(fp, "KEY_QUIT %d\n", keymap.quit);
+	fprintf(fp, "KEY_PAUSE %d\n", keymap.pause);
 	fclose(fp);
 	return 1;
 }
