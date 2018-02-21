db92dd4810e7af5d36139191a1f9745562776ff3
Ivan <hi-ban@hotmail.com>
2018-02-21 02:02:56 +0100

    GCW0: Small fix for ingame menu resolution

--- src/sdl-dingux/sdl_menu.cpp
+++ src/sdl-dingux/sdl_menu.cpp
@@ -375,7 +375,7 @@ void gui_Run()
 #endif
 	gettimeofday(&s, NULL);
 #ifdef DEVICE_GCW0
-	if (hwscale > 0 && screen->w != 320 && screen->h != 240) {
+	if (hwscale > 0 && (screen->w != 320 || screen->h != 240)) {
 		VideoInitForce320x240(); // sets video mode to 320x240 so the menu screen looks right when a game uses a resolution different than 320x240
 	}
 #endif	
